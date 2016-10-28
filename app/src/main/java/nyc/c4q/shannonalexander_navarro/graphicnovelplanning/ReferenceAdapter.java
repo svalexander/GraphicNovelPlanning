9b5447c58160d1ed9ead8a4f37945cf522707f56
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2016-10-28 10:38:24 -0400

    first commit

---
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/graphicnovelplanning/ReferenceAdapter.java
@@ -0,0 +1,25 @@
+package nyc.c4q.shannonalexander_navarro.graphicnovelplanning;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by shannonalexander-navarro on 10/27/16.
+ */
+
+public class ReferenceAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/graphicnovelplanning/ReferenceViewHolder.java
@@ -0,0 +1,15 @@
+package nyc.c4q.shannonalexander_navarro.graphicnovelplanning;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by shannonalexander-navarro on 10/27/16.
+ */
+
+public class ReferenceViewHolder  extends RecyclerView{
+    public ReferenceViewHolder(Context context) {
+        super(context);
+    }
+
+}
